73f837e335b3dc1b91739b9431dba5dae8209dad
Person552 <qaustinbarnes@gmail.com>
2025-12-09 00:57:01 -0800

    add new materials to sniffer loot

--- src/main/java/me/dinoson850/ancientweapons/AncientWeapons.java
+++ src/main/java/me/dinoson850/ancientweapons/AncientWeapons.java
@@ -3,6 +3,13 @@ package me.dinoson850.ancientweapons;
 import me.dinoson850.ancientweapons.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,5 +28,13 @@ public class AncientWeapons implements ModInitializer {
 		// Proceed with mild caution.
 
 		ModItems.registerModItems();
+		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+			if (source.isBuiltin() && Identifier.of("minecraft", "gameplay/sniffer_digging").equals(id)) {
+				LootPool.Builder poolBuilder = LootPool.builder()
+						.with(ItemEntry.builder(ModItems.ANCIENT_BONE))
+						.with(ItemEntry.builder(ModItems.ANCIENT_FANG));
+				tableBuilder.pool(poolBuilder);
+			}
+		});
 	}
 }
